6726f91a7aee428d7dec5f01244c9016626c2661
Yash Mahalwal <yashmahalwal@gmail.com>
2020-08-03 22:38:58 +0530

    feat: abstracted firebase user fetching}

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/ISTE-SC-MANIT/megatreopuz-models
 
 go 1.14
 
-require go.mongodb.org/mongo-driver v1.3.5
+require (
+	firebase.google.com/go v3.13.0+incompatible // indirect
+	go.mongodb.org/mongo-driver v1.3.5
+	google.golang.org/api v0.29.0 // indirect
+	google.golang.org/grpc v1.31.0 // indirect
+)
---
+++ utils/getUser.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+
+	"firebase.google.com/go/auth"
+	"google.golang.org/grpc/metadata"
+)
+
+// GetUserFromFirebase takes grpc metadata and fetches the user from firebase
+func GetUserFromFirebase(ctx context.Context, a *auth.Client) (*auth.Token, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, fmt.Errorf("not able to extract metadata")
+	}
+	accessTokenSlice, ok := md["authorization"]
+	if !ok {
+		return nil, fmt.Errorf("invalid access token")
+	}
+
+	value := accessTokenSlice[0]
+
+	decoded, err := a.VerifySessionCookieAndCheckRevoked(ctx, value)
+	if err != nil {
+		return nil, fmt.Errorf("cookie could not be verified")
+	}
+
+	return decoded, nil
+}
