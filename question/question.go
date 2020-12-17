392f9fced832a7f5b215a578d6dc5fca3446a215
Devansh Sharma <kdevanshsharma23@gmail.com>
2020-12-18 00:06:30 +0530

    fix: question

--- question/question.go
+++ question/question.go
@@ -1,9 +1,7 @@
 package question
 
-
-
 // Question : The struct to map to question collection
-type User struct {
+type Question struct {
 	ID         string `bson:"_id,omitempty"`
 	Answer     string `bson:"answer"`
 	QuestionNo string `bson:"questionNo"`
