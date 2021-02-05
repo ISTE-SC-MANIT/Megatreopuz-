8d07d2158a1fce76ce7d6e5ef9b858f06e7145a2
Devansh Sharma <kdevanshsharma23@gmail.com>
2021-02-05 13:54:44 +0530

    chore: commented unused code

--- auth/server.go
+++ auth/server.go
@@ -2,7 +2,7 @@ package auth
 
 import (
 	"firebase.google.com/go/v4/auth"
-	"github.com/go-redis/redis/v8"
+	// "github.com/go-redis/redis/v8"
 )
 
 // Server is struct for the auth grpc server
@@ -10,5 +10,5 @@ type Server struct {
 	FirebaseApp FirebaseInteraction
 	MongoClient MongoDBInteraction
 	AuthClient  *auth.Client
-	RedisClient *redis.Client
+	// RedisClient *redis.Client
 }
--- go.mod
+++ go.mod
@@ -8,8 +8,7 @@ require (
 	firebase.google.com/go/v4 v4.0.0
 	github.com/ISTE-SC-MANIT/megatreopuz-models v0.0.0-20201217175211-de22897a5f9c
 	github.com/bxcodec/faker/v3 v3.5.0
-	github.com/go-redis/redis/v8 v8.5.0
-	// github.com/go-redis/redis/v8 v8.0.0-beta.7
+
 	github.com/golang/protobuf v1.4.2
 	github.com/joho/godotenv v1.3.0
 	github.com/onsi/ginkgo v1.15.0
